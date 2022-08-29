29fec2513264bc92f274b4fc7c20cfb186cbde00
Xifre Font <xifre.font.97@gmail.com>
2022-08-29 21:18:48 +0200

    Replaced default jest render with custom one

---
+++ src/test-utils/render/customRender.ts
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import Wrapper from "./Wrapper";
+
+const customRender = (view: JSX.Element, ...options: any) =>
+  render(view, { wrapper: Wrapper, ...options });
+
+export * from "@testing-library/react";
+
+export { customRender as render };
