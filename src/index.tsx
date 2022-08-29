f1d4a37b483455cd5bb6e44ea6e05f9fdf0170d0
Xifre Font <xifre.font.97@gmail.com>
2022-08-29 19:49:35 +0200

    Installed styled components, set css variables and wrapped the app

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
+import { ThemeProvider } from "styled-components";
+import styledMainTheme from "./styledMainTheme";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,7 +13,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={styledMainTheme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
