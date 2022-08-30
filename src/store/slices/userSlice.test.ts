af873e0c30e8324f0bab7cfa94e569ccc7c47e66
Xifre Font <xifre.font.97@gmail.com>
2022-08-30 21:01:15 +0200

    Tested the toggle action creator and its functionality on the store

--- src/store/slices/userSlice.test.ts
+++ src/store/slices/userSlice.test.ts
@@ -1,9 +1,14 @@
 import mockUser from "../../test-utils/mocks/mockUser";
-import { signInActionCreator, userReducer } from "./userSlice";
+import { IUser } from "../../types/user";
+import {
+  signInActionCreator,
+  toggleStatusActionCreator,
+  userReducer,
+} from "./userSlice";
 
 describe("Given a signInUser action creator", () => {
   describe("When called with a user as a payload", () => {
-    test("Then it should return an action with a type 'asdfasdf' and the user as payload", () => {
+    test("Then it should return an action with a type 'user/signIn' and the user as payload", () => {
       const actionType = "user/signIn";
       const expectedAction = {
         type: actionType,
@@ -17,14 +22,50 @@ describe("Given a signInUser action creator", () => {
   });
 });
 
+describe("Given a toggle status action creator", () => {
+  describe("When called", () => {
+    test("Then it should return an action with a type 'user/toggleStatus'", () => {
+      const actionType = "user/toggleStatus";
+      const expectedAction = {
+        type: actionType,
+        payload: undefined,
+      };
+
+      const action = toggleStatusActionCreator();
+
+      expect(action).toStrictEqual(expectedAction);
+    });
+  });
+});
+
 describe("Given a userReducer function", () => {
+  const previousState = { user: {} as IUser, isLogged: false };
+
   describe("When called with a signUp action with a new user as a payload", () => {
-    test("Then it should return a new user", () => {
-      const previousState = { user: mockUser, isLogged: true };
+    test("Then it should replace the previous user with the passed one", () => {
+      const expectedResult = {
+        ...previousState,
+        user: mockUser,
+      };
       const action = signInActionCreator(mockUser);
 
       const result = userReducer(previousState, action);
-      expect(result).toStrictEqual(previousState);
+
+      expect(result).toStrictEqual(expectedResult);
+    });
+  });
+
+  describe("When called with a toggleStatus action", () => {
+    test("Then it should return toggle 'isLogged' from false to true", () => {
+      const expectedResult = {
+        ...previousState,
+        isLogged: true,
+      };
+      const action = toggleStatusActionCreator();
+
+      const result = userReducer(previousState, action);
+
+      expect(result).toStrictEqual(expectedResult);
     });
   });
 });
--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -14,6 +14,7 @@ const userSlice = createSlice({
       ...previousState,
       isLogged: !previousState.isLogged,
     }),
+
     signIn: (previousState, action: PayloadAction<IUser>) => ({
       ...previousState,
       user: action.payload,
