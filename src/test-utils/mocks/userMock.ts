e82c2069995b01aa01a73f8012c7da222778599e
Xifre Font <xifre.font.97@gmail.com>
2022-08-30 09:24:14 +0200

    Created a mock of a user for testing purposes

---
+++ src/test-utils/mocks/userMock.ts
@@ -0,0 +1,10 @@
+import { IUser } from "../../types/user";
+
+const userMock: IUser = {
+  id: "id",
+  name: "name",
+  email: "email@email.com",
+  friends: ["friend_one", "friend_two"],
+};
+
+export default userMock;
