e2a7b83685d8f03e07fe29c878ca664a7de4c663
Xifre Font <xifre.font.97@gmail.com>
2022-08-31 18:53:01 +0200

    Created the modal slice and a toggleVisibility action

---
+++ src/store/slices/uiModal/uiModalSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const uiModalInitialState = {
+  isVisible: false,
+  message: "",
+  type: "loading",
+};
+
+const uiModalSlice = createSlice({
+  name: "uiModal",
+  initialState: uiModalInitialState,
+  reducers: {
+    toggleVisibility: (previousState, action: PayloadAction<boolean>) => ({
+      ...previousState,
+      isVisible: action.payload,
+    }),
+  },
+});
+
+export const uiModalReducer = uiModalSlice.reducer;
+
+export const { toggleVisibility: toggleVisibilityActionCreator } =
+  uiModalSlice.actions;
