c6719b7941686212a0833893e9f0e05b91e0bce6
Xifre Font <xifre.font.97@gmail.com>
2022-09-02 17:56:31 +0200

    Implemented projects being loaded

--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -1,14 +1,27 @@
+import { useEffect } from "react";
+import { useAppSelector } from "../../app/hooks";
+import { RootState } from "../../app/store";
 import Projects from "../../components/Projects/Projects";
-import mockProject from "../../test-utils/mocks/mockProject";
+import useProjects from "../../hooks/useProjects/useProjects";
 
-const ExplorePage = (): JSX.Element => (
-  <>
-    <h2 className="page__title">
-      These are the <span className="page__title--bold">latest projects</span>
-    </h2>
+const ExplorePage = (): JSX.Element => {
+  const { getAll } = useProjects();
 
-    <Projects projects={[mockProject, mockProject]} />
-  </>
-);
+  useEffect(() => {
+    getAll();
+  }, [getAll]);
+
+  const allProjects = useAppSelector((state: RootState) => state.projects);
+
+  return (
+    <>
+      <h2 className="page__title">
+        These are the <span className="page__title--bold">latest projects</span>
+      </h2>
+
+      <Projects projects={allProjects} />
+    </>
+  );
+};
 
 export default ExplorePage;
