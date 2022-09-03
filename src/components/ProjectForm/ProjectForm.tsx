cecb86a055a46cea4d988bcb45590ebf3db2cfb4
Xifre Font <xifre.font.97@gmail.com>
2022-09-03 19:09:31 +0200

    Added functionality to the form

--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   GroupStyled,
   InputStyled,
@@ -10,76 +11,112 @@ interface ProjectFormProps {
   isCreate: boolean;
 }
 
-const ProjectForm = ({ isCreate }: ProjectFormProps): JSX.Element => (
-  <SignFormStyled>
-    <h3 className="form__title">
-      {isCreate ? "Tell us about your project" : "Update your project"}
-    </h3>
+const initialState = {
+  name: "",
+  repository: "",
+  logo: "",
+  technologyFront: "",
+  technologyBack: "",
+  description: "",
+};
 
-    <GroupStyled>
-      <LabelStyled htmlFor="name">Name</LabelStyled>
-      <InputStyled
-        type="text"
-        id="name"
-        placeholder="John Doe"
-        autoComplete="off"
+const ProjectForm = ({ isCreate }: ProjectFormProps): JSX.Element => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setValues({
+      ...values,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <SignFormStyled>
+      <h3 className="form__title">
+        {isCreate ? "Tell us about your project" : "Update your project"}
+      </h3>
+
+      <GroupStyled>
+        <LabelStyled htmlFor="name">Name</LabelStyled>
+        <InputStyled
+          type="text"
+          id="name"
+          placeholder="John Doe"
+          autoComplete="off"
+          value={values.name}
+          onChange={handleChange}
+        />
+      </GroupStyled>
+
+      <GroupStyled>
+        <LabelStyled htmlFor="name">Repository URL</LabelStyled>
+        <InputStyled
+          type="text"
+          id="repository"
+          placeholder="John Doe"
+          autoComplete="off"
+          value={values.repository}
+          onChange={handleChange}
+        />
+      </GroupStyled>
+
+      <GroupStyled>
+        <LabelStyled htmlFor="logo">Project logo</LabelStyled>
+        <InputStyled
+          type="file"
+          id="logo"
+          autoComplete="off"
+          value={values.logo}
+          onChange={handleChange}
+        />
+      </GroupStyled>
+
+      <GroupStyled>
+        <LabelStyled htmlFor="technologyFront">
+          Frontend main library or framework
+        </LabelStyled>
+        <InputStyled
+          type="text"
+          id="technologyFront"
+          placeholder="React"
+          autoComplete="off"
+          value={values.technologyFront}
+          onChange={handleChange}
+        />
+      </GroupStyled>
+
+      <GroupStyled>
+        <LabelStyled htmlFor="technologyBack">
+          Backend main library or framework
+        </LabelStyled>
+        <InputStyled
+          type="text"
+          id="technologyBack"
+          placeholder="Express"
+          autoComplete="off"
+          value={values.technologyBack}
+          onChange={handleChange}
+        />
+      </GroupStyled>
+
+      <GroupStyled>
+        <LabelStyled htmlFor="name">Description</LabelStyled>
+        <InputStyled
+          type="text"
+          id="description"
+          placeholder="Music app is a wonderful system to share music between its users."
+          autoComplete="off"
+          value={values.description}
+          onChange={handleChange}
+        />
+      </GroupStyled>
+
+      <Button
+        content={isCreate ? "Create project" : "Update project"}
+        type="submit"
       />
-    </GroupStyled>
-
-    <GroupStyled>
-      <LabelStyled htmlFor="name">Repository URL</LabelStyled>
-      <InputStyled
-        type="text"
-        id="repository"
-        placeholder="John Doe"
-        autoComplete="off"
-      />
-    </GroupStyled>
-
-    <GroupStyled>
-      <LabelStyled htmlFor="name">Project logo</LabelStyled>
-      <InputStyled type="file" id="name" autoComplete="off" />
-    </GroupStyled>
-
-    <GroupStyled>
-      <LabelStyled htmlFor="technologyFront">
-        Frontend main library or framework
-      </LabelStyled>
-      <InputStyled
-        type="text"
-        id="technologyFront"
-        placeholder="React"
-        autoComplete="off"
-      />
-    </GroupStyled>
-
-    <GroupStyled>
-      <LabelStyled htmlFor="technologyBack">
-        Backend main library or framework
-      </LabelStyled>
-      <InputStyled
-        type="text"
-        id="technologyBack"
-        placeholder="Express"
-        autoComplete="off"
-      />
-    </GroupStyled>
-
-    <GroupStyled>
-      <LabelStyled htmlFor="name">Description</LabelStyled>
-      <InputStyled
-        type="text"
-        id="name"
-        placeholder="Music app is a wonderful system to share music between its users."
-        autoComplete="off"
-      />
-    </GroupStyled>
-
-    <Button
-      content={isCreate ? "Create project" : "Update project"}
-      type="submit"
-    />
-  </SignFormStyled>
-);
+    </SignFormStyled>
+  );
+};
 
 export default ProjectForm;
