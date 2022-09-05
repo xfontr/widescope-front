cc27b9951460a0d855a8fef9acaf8dca24989fdd
Xifre Font <xifre.font.97@gmail.com>
2022-09-05 10:56:06 +0200

    Tested the load user projects action creator

--- src/store/slices/userData/userDataSlice.test.ts
+++ src/store/slices/userData/userDataSlice.test.ts
@@ -1,10 +1,15 @@
+import mockProject from "../../../test-utils/mocks/mockProject";
 import { mockUserExtraData } from "../../../test-utils/mocks/mockUserData";
 import { UserExtraData } from "../../../types/user";
-import { loadUserDataActionCreator, userDataReducer } from "./userDataSlice";
+import {
+  loadUserDataActionCreator,
+  loadUserProjectsActionCreator,
+  userDataReducer,
+} from "./userDataSlice";
 
-describe("Given a loadUserData action creator", () => {
+describe("Given a loadUserDataActionCreator", () => {
   describe("When called with a user as a payload", () => {
-    test("Then it should return an action with a type 'user/signIn' and the user as payload", () => {
+    test("Then it should return an action with a type 'userData/signIn' and the user as payload", () => {
       const actionType = "userData/loadUserData";
       const expectedAction = {
         type: actionType,
@@ -18,8 +23,24 @@ describe("Given a loadUserData action creator", () => {
   });
 });
 
+describe("Given a loadUserProjectsActionCreator", () => {
+  describe("When called with a list of projects as a payload", () => {
+    test("Then it should return an action with a type userData/loadUserProjects and said list as payload", () => {
+      const actionType = "userData/loadUserProjects";
+      const expectedAction = {
+        type: actionType,
+        payload: [mockProject],
+      };
+
+      const action = loadUserProjectsActionCreator([mockProject]);
+
+      expect(action).toStrictEqual(expectedAction);
+    });
+  });
+});
+
 describe("Given a userDataReducer function", () => {
-  describe("When called with a loadUserData action with a new user as a payload", () => {
+  describe("When called with a loadUser action with a new user as a payload", () => {
     test("Then it should replace the previous user with the passed one", () => {
       const previousState = { userData: {} as UserExtraData };
       const expectedResult = {
@@ -27,6 +48,24 @@ describe("Given a userDataReducer function", () => {
         userData: mockUserExtraData,
       };
       const action = loadUserDataActionCreator(mockUserExtraData);
+
+      const result = userDataReducer(previousState, action);
+
+      expect(result).toStrictEqual(expectedResult);
+    });
+  });
+
+  describe("When called with a loadUserProjects action with a list of projects as a payload", () => {
+    test("Then it should replace the previous user project list with the passed one", () => {
+      const previousState = { userData: mockUserExtraData };
+      const expectedResult = {
+        ...previousState,
+        userData: {
+          ...mockUserExtraData,
+          projects: [mockProject, mockProject],
+        },
+      };
+      const action = loadUserProjectsActionCreator([mockProject, mockProject]);
 
       const result = userDataReducer(previousState, action);
 
