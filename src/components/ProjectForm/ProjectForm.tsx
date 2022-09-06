7066a224f62ec9087aa338ffb82d0635fef12420
Xifre Font <xifre.font.97@gmail.com>
2022-09-06 16:41:23 +0200

    Project form to call update on submit if it's update type

--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -1,6 +1,8 @@
 import { SyntheticEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
 import { RootState } from "../../app/store";
+import routes from "../../configs/routes";
 import useProjects from "../../hooks/useProjects/useProjects";
 import projectSchema from "../../schemas/projectSchema";
 import {
@@ -11,28 +13,32 @@ import {
   LabelStyled,
   SignFormStyled,
 } from "../../styles/FormStyled";
+import { IProject } from "../../types/project";
 import Button from "../Button/Button";
 
 interface ProjectFormProps {
   isCreate: boolean;
+  project?: IProject;
 }
-
-const initialState = {
-  name: "",
-  repository: "",
-  logo: "",
-  technologyFront: "",
-  technologyBack: "",
-  description: "",
-};
 
 const formData = new FormData();
 
-const ProjectForm = ({ isCreate }: ProjectFormProps): JSX.Element => {
-  const [values, setValues] = useState(initialState);
-  const { create } = useProjects();
+const ProjectForm = ({ isCreate, project }: ProjectFormProps): JSX.Element => {
+  const { create, update } = useProjects();
   const user = useAppSelector((state: RootState) => state.user.user);
   const [errors, setErrors] = useState([] as string[]);
+  const navigate = useNavigate();
+
+  const initialState = {
+    name: project ? project.name : "",
+    repository: project ? project.repository : "",
+    logo: project ? project.logo : "",
+    technologyFront: project ? project.technologies[0] : "",
+    technologyBack: project ? project.technologies[1] : "",
+    description: project ? project.description : "",
+  };
+
+  const [values, setValues] = useState(initialState);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setValues({
@@ -74,6 +80,16 @@ const ProjectForm = ({ isCreate }: ProjectFormProps): JSX.Element => {
     description: values.description,
   });
 
+  const clearForm = () => {
+    formData.delete("logo");
+    formData.delete("project");
+
+    setValues(initialState);
+    setErrors([]);
+
+    navigate(routes.personalProjects);
+  };
+
   const handleSubmit = async (event: SyntheticEvent): Promise<void> => {
     event.preventDefault();
 
@@ -81,22 +97,26 @@ const ProjectForm = ({ isCreate }: ProjectFormProps): JSX.Element => {
       return;
     }
 
+    if (initialState.logo === values.logo) {
+      formData.delete("logo");
+    }
+
     formData.append("project", JSON.stringify(curateData()));
 
-    await create(formData);
+    if (isCreate) {
+      await create(formData);
+    } else {
+      await update(formData);
+    }
 
-    formData.delete("logo");
-    formData.delete("project");
-
-    setValues(initialState);
-    setErrors([]);
+    clearForm();
   };
 
   return (
     <SignFormStyled onSubmit={handleSubmit} data-testid="form">
       <HeaderStyled>
         <h3 className="form__title">
-          {isCreate ? "Tell us about your project" : "Update your project"}
+          {isCreate ? "Tell us about your project" : `Update ${project!.name}`}
         </h3>
       </HeaderStyled>
 
