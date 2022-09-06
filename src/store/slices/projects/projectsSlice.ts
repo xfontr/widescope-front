acb15412844f98e3a40ff9a9673e3275cb23ac1a
Xifre Font <xifre.font.97@gmail.com>
2022-09-06 14:07:51 +0200

    Update action creator for the personal list, and tested it

--- src/store/slices/projects/projectsSlice.ts
+++ src/store/slices/projects/projectsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, current, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IProject, Projects } from "../../../types/project";
 
 const projectsInitialState: Projects = [];
