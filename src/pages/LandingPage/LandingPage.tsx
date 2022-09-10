fed67433ee2db3362df246e518f48dff08f36fd4
Xifre Font <xifre.font.97@gmail.com>
2022-09-10 15:41:05 +0200

    Landgin page skeleton and functionality

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { useAppSelector } from "../../app/hooks";
+import Button from "../../components/Button/Button";
+import useProjects from "../../hooks/useProjects/useProjects";
+import { CTASectionStyled, HeroSectionStyled } from "./LandingPageStyled";
+import Projects from "../../components/Projects/Projects";
+import SignForm from "../../components/SignForm/SignForm";
+
+const LandingPage = (): JSX.Element => {
+  const { getAll } = useProjects();
+
+  useEffect(() => {
+    (async () => {
+      await getAll(0);
+    })();
+  }, [getAll]);
+
+  const projects = useAppSelector(({ projects }) =>
+    projects.filter((project, index) => index < 6)
+  );
+  const isLogged = useAppSelector(({ user }) => user.isLogged);
+
+  return (
+    <>
+      <HeroSectionStyled>
+        <h1 className="page__title page__title--landing">
+          Let them know
+          <span className="page__title--bold"> what you've got</span>
+        </h1>
+
+        <p className="page__title-subheading page__title-subheading--landing">
+          Set your projects to a new level by sharing them with wideScope's
+          glowing community of developers.
+        </p>
+
+        <Button>Get started</Button>
+        <Button customStyle="outline">Explore</Button>
+      </HeroSectionStyled>
+
+      <section className="langing-page__projects">
+        <Projects projects={projects}></Projects>
+      </section>
+
+      <CTASectionStyled>
+        {!isLogged && (
+          <div className="landing-page__log-in">
+            <div className="cta-section">
+              <h2 className="page__title">
+                Start sharing your projects. <br />
+                <span className="page__title--bold"> It's free</span>
+              </h2>
+            </div>
+
+            <SignForm isLogin={true} />
+          </div>
+        )}
+
+        {isLogged && (
+          <div className="landing-page__create">
+            <div className="cta-section">
+              <h2 className="page__title">
+                Start sharing your projects. <br />
+                <span className="page__title--bold"> It's free</span>
+              </h2>
+
+              <Button customStyle="outline-invert">Post a project</Button>
+            </div>
+          </div>
+        )}
+      </CTASectionStyled>
+    </>
+  );
+};
+
+export default LandingPage;
