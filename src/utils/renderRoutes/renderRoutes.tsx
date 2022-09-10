57789da2b7c36dc5414660db3db4683e2cbf66ab
Xifre Font <xifre.font.97@gmail.com>
2022-09-10 11:59:45 +0200

    Added a key to all the routes rendered

--- src/utils/renderRoutes/renderRoutes.tsx
+++ src/utils/renderRoutes/renderRoutes.tsx
@@ -2,38 +2,51 @@ import { Route } from "react-router-dom";
 import Validator from "../../components/Validator/Validator";
 import { NavRoute, navRoutes } from "../../configs/routes";
 
-const renderRoutes = (isLogged: boolean, route: NavRoute): JSX.Element => {
+const renderRoutes = (
+  isLogged: boolean,
+  route: NavRoute
+): JSX.Element | undefined => {
   const element = route.render && route.render();
 
   if (!element) {
-    return <></>;
+    return undefined;
   }
 
   switch (route.display) {
     case "notLogged":
       return (
         <Route
+          key={`validator${route.path}`}
           element={
-            <Validator option={!isLogged} rejectPath={navRoutes.explore.path} />
+            <Validator
+              key={`val${route.path}`}
+              option={!isLogged}
+              rejectPath={navRoutes.explore.path}
+            />
           }
         >
-          <Route path={route.path} element={element} />
+          <Route key={route.path} path={route.path} element={element} />
         </Route>
       );
 
     case "logged":
       return (
         <Route
+          key={`validator${route.path}`}
           element={
-            <Validator option={isLogged} rejectPath={navRoutes.logIn.path} />
+            <Validator
+              key={`val${route.path}`}
+              option={isLogged}
+              rejectPath={navRoutes.logIn.path}
+            />
           }
         >
-          <Route path={route.path} element={element} />
+          <Route key={route.path} path={route.path} element={element} />
         </Route>
       );
 
     default:
-      return <Route path={route.path} element={element} />;
+      return <Route key={route.path} path={route.path} element={element} />;
   }
 };
 
