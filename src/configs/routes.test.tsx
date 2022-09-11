7f16cf0472257201f4fc7be6946dff91ae0b564a
Xifre Font <xifre.font.97@gmail.com>
2022-09-11 18:52:28 +0200

    Tested the configs route generator

---
+++ src/configs/routes.test.tsx
@@ -0,0 +1,48 @@
+import mockProject from "../test-utils/mocks/mockProject";
+import { render, screen } from "../test-utils/render/customRender";
+import { navRoutes } from "./routes";
+
+describe("asdf", () => {
+  describe("asdf", () => {
+    test("asdfasdf", async () => {
+      render(
+        <>
+          {Object.values(navRoutes).map(
+            (route) => route.render && route.render()
+          )}
+        </>
+      );
+
+      const pages = [
+        await screen.findByRole("heading", {
+          name: "These are the latest projects",
+        }),
+        await screen.findByRole("heading", {
+          name: "Sign up",
+          level: 3,
+        }),
+        await screen.findByRole("heading", {
+          name: "Oops! We couldn't find what you are looking for",
+          level: 2,
+        }),
+
+        await screen.findByRole("heading", {
+          name: "Your creations. Thanks for sharing!",
+        }),
+      ];
+
+      const logInPage = await screen.findAllByRole("heading", {
+        name: "Log in",
+        level: 3,
+      });
+
+      const projectDetailsPage = await screen.findAllByRole("heading", {
+        name: mockProject.name,
+      });
+
+      pages.forEach((element) => expect(element).toBeInTheDocument());
+      expect(logInPage).toHaveLength(2);
+      expect(projectDetailsPage).toHaveLength(2);
+    });
+  });
+});
